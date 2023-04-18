9f56e2b8b4705c29b6f87dae90286aa2818f3240
yonatan gershfeld <yonatan310867@gmail.com>
2023-04-18 14:22:38 +0300

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (mystring) {
+    let stringnew ='';
+    for(let i =mystring.length ; i>0; i--){
+        stringnew = stringnew + mystring.substr( (-i), 1);
+    }
+    if(stringnew == mystring){
+        return true;
+    }else return false
 };
 
 // Do not edit below this line
