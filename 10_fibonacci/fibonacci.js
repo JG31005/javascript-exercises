beab1fef303392f6596b4b6ebeb96de6d48e7c24
yonatan gershfeld <yonatan310867@gmail.com>
2023-04-24 13:34:57 +0300

    prototype fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacci = [1,1];
+    fibonacci.length = num;
+    for(let i =3; i<fibonacci.length; i++){
+        fibonacci[i]= fibonacci[i-1]+ fibonacci[i-2];
+    }
+    return fibonacci[num];
 
 };
 
