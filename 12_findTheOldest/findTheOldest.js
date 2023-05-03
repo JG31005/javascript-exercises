87c721c0914cadabde9e5b0d453957465a13e1a3
yonatan gershfeld <yonatan310867@gmail.com>
2023-05-03 13:34:35 +0300

    add oldest if alive check

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,12 @@
 const findTheOldest = function(pplarr) {
-    
+    const dte = new Date();
 const oldest = pplarr.reduce((accumulator,currrentvalue) =>{
-
+    if(accumulator.yearOfDeath == undefined){
+        accumulator.yearOfDeath = dte.getfullyear();
+    }
+    if(currrentvalue.yearOfDeath == undefined){
+        currrentvalue.yearOfDeath =dte.getfullyear();
+    }
     age1= accumulator.yearOfDeath - accumulator.yearOfBirth;
     age2 = currrentvalue.yearOfDeath - currrentvalue.yearOfBirth;
     return age2 > age1 ? currrentvalue : accumulator;
