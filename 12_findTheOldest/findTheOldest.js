323c919a579903c62b619057f5f07f58fa36068d
yonatan gershfeld <yonatan310867@gmail.com>
2023-05-03 13:23:48 +0300

    IT WORKED

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(pplarr) {
+const oldest = pplarr.reduce((accumulator,currrentvalue) =>{
+    age1= accumulator.yearOfDeath - accumulator.yearOfBirth;
+    age2 = currrentvalue.yearOfDeath - currrentvalue.yearOfBirth;
+    return age2 > age1 ? currrentvalue : accumulator;
+})
+return oldest;
 };
 
 // Do not edit below this line
